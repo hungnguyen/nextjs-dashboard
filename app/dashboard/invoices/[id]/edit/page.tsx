69b80ff3fbcd9748a928c4c4b344986277b09526
hungnguyen <billy_hung85@yahoo.com>
2024-03-11 13:44:08 +0700

    fix build fail

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { fetchCustomers } from "@/app/lib/customer-data";
+import {fetchInvoiceById} from "@/app/lib/data"
 import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
